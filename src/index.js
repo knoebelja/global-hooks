7b6b0fd35adcbfdee3506eb0a9882c4abe8fb2d5
Joseph Knoebel <knoebelja@gmail.com>
2019-03-16 15:38:49 -0400

    Rename GlobalStoreProvider to StoreProvider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import GlobalStoreProvider from "./store";
+import StoreProvider from "./store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <GlobalStoreProvider>
+  <StoreProvider>
     <App />
-  </GlobalStoreProvider>,
+  </StoreProvider>,
   document.getElementById("root")
 );
 
